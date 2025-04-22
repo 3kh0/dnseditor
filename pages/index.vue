a6e0dbe87b846a2cd8470c5ec24e225cc18c7e50
Echo <github@3kh0.net>
2025-04-22 14:15:19 -0400

    meow

--- pages/index.vue
+++ pages/index.vue
@@ -25,19 +25,50 @@
           <span class="line-clamp-1">{{ domain.replace(".yaml", "") }}</span>
         </button>
       </div>
+      <p class="text-sm text-muted mt-4">
+        Made by
+        <a href="https://3kh0.net" target="_blank" class="text-primary">3kh0</a>
+      </p>
     </aside>
 
     <!-- Main Content -->
     <main class="flex-1 p-8 overflow-auto">
       <div class="max-w-6xl mx-auto">
-        <h1
-          class="text-4xl font-bold mb-8 text-primary flex items-center gap-3"
-        >
-          <Icon name="material-symbols:dns" class="w-8 h-8" />
-          {{ selectedDomain.replace(".yaml", "") }}
-        </h1>
+        <div class="flex flex-col gap-6 relative items-center">
+          <h1 class="text-4xl font-bold text-primary flex items-center gap-3">
+            <Icon name="material-symbols:dns" class="w-8 h-8" />
+            {{ selectedDomain.replace(".yaml", "") }}
+          </h1>
 
-        <div v-if="loading" class="flex items-center justify-center p-2">
+          <!-- Search Bar -->
+          <div
+            class="w-full pl-10 pr-4 py-3 rounded-lg bg-darkless/40 backdrop-blur-md border border-border/20 text-snow placeholder-muted/60 focus:outline-none focus:ring-2 focus:ring-primary/50 transition-all duration-300"
+          >
+            <Icon
+              name="material-symbols:search-rounded"
+              class="text-muted absolute left-3 pointer-events-none"
+              size="1.5em"
+            />
+            <!-- eslint-disable-next-line vue/html-self-closing -->
+            <input
+              v-model="searchQuery"
+              type="text"
+              placeholder="Search records and values..."
+              class="w-full bg-transparent focus:outline-none"
+            />
+          </div>
+        </div>
+
+        <!-- X records found -->
+
+        <div v-if="filteredRecords.length > 0" class="text-muted text-sm mt-4">
+          {{ filteredRecords.length }} records found
+          <span v-if="searchQuery" class="text-primary">
+            (searching for "{{ searchQuery }}")
+          </span>
+        </div>
+
+        <div v-if="loading" class="flex items-center justify-center p-12">
           <div class="text-primary">
             <Icon name="codex:loader" size="4em" />
           </div>
@@ -51,9 +82,16 @@
           {{ error }}
         </div>
 
-        <div v-else class="space-y-4">
+        <div v-else class="space-y-4 mt-6">
           <div
-            v-for="(recordGroup, index) in records"
+            v-if="filteredRecords.length === 0"
+            class="text-center text-muted py-12"
+          >
+            No matching records or values found :(
+          </div>
+
+          <div
+            v-for="(recordGroup, index) in filteredRecords"
             :key="index"
             class="group rounded-xl border border-border/10 bg-darkless/40 backdrop-blur-md shadow-lg transition-all duration-300 hover:shadow-xl hover:bg-darkless/60"
           >
@@ -68,7 +106,9 @@
                   name="material-symbols:settop-component-rounded"
                   class="w-5 h-5"
                 />
-                {{ trimSubdomain(recordGroup.subdomain) }}
+                <span
+                  v-html="highlightMatch(trimSubdomain(recordGroup.subdomain))"
+                ></span>
               </h2>
               <Icon
                 name="icon-park-outline:down"
@@ -100,7 +140,7 @@
                       class="group/row hover:bg-darkless/80"
                     >
                       <td class="px-6 py-4 text-sm font-medium">
-                        {{ record.type }}
+                        <span v-html="highlightMatch(record.type)"></span>
                       </td>
                       <td class="px-6 py-4 text-sm">
                         <div v-if="Array.isArray(record.values)">
@@ -112,23 +152,107 @@
                             >
                               <template v-if="record.type === 'MX'">
                                 <span class="text-muted">Priority:</span>
-                                {{ value.priority }},
+                                <span
+                                  v-if="
+                                    value &&
+                                    ('priority' in value ||
+                                      'preference' in value)
+                                  "
+                                  v-html="
+                                    highlightMatch(
+                                      (
+                                        value.priority || value.preference
+                                      ).toString()
+                                    )
+                                  "
+                                ></span>
+                                <span v-else class="text-yellow"
+                                  >Invalid Priority</span
+                                >,
                                 <span class="text-muted">Exchange:</span>
-                                {{ value.exchange }}
+                                <span
+                                  v-if="value && 'exchange' in value"
+                                  v-html="highlightMatch(value.exchange)"
+                                ></span>
+                                <span v-else class="text-yellow"
+                                  >Invalid Exchange</span
+                                >
                               </template>
-                              <template v-else-if="record.type === 'SSHFP'">
-                                <span class="text-muted">Algorithm:</span>
-                                {{ value.algorithm }},
-                                <span class="text-muted">Type:</span>
-                                {{ value.fingerprint_type }},
-                                <span class="text-muted">Fingerprint:</span>
-                                {{ value.fingerprint }}
+                              <template
+                                v-else-if="
+                                  record.type === 'CNAME' &&
+                                  isVercelDomain(value)
+                                "
+                              >
+                                <span class="flex items-center gap-2">
+                                  <Icon
+                                    name="simple-icons:vercel"
+                                    class="w-4 h-4"
+                                  />
+                                  <span class="text-muted"
+                                    >Deployed on Vercel</span
+                                  >
+                                </span>
                               </template>
-                              <template v-else>{{ value }}</template>
+                              <template v-else-if="record.type === 'TXT'">
+                                <span
+                                  class="font-mono text-sm"
+                                  v-html="highlightMatch(value)"
+                                ></span>
+                              </template>
+                              <template v-else>
+                                <span
+                                  v-html="highlightMatch(value.toString())"
+                                ></span>
+                              </template>
                             </li>
                           </ul>
                         </div>
-                        <div v-else>{{ record.values }}</div>
+                        <div v-else>
+                          <template
+                            v-if="
+                              record.type === 'MX' && isMXRecord(record.value)
+                            "
+                          >
+                            <span class="text-muted">Priority: </span>
+                            <span
+                              v-html="
+                                highlightMatch(
+                                  (
+                                    record.value.priority ||
+                                    record.value.preference
+                                  ).toString()
+                                )
+                              "
+                            ></span
+                            >,
+                            <span class="text-muted">Exchange: </span>
+                            <span
+                              v-html="highlightMatch(record.value.exchange)"
+                            ></span>
+                          </template>
+                          <template
+                            v-else-if="
+                              (record.type === 'CNAME' ||
+                                record.type === 'ALIAS') &&
+                              isVercelDomain(record.values)
+                            "
+                          >
+                            <span class="flex items-center gap-2">
+                              <Icon
+                                name="simple-icons:vercel"
+                                class="w-4 h-4"
+                              />
+                              <span class="">Deployed on Vercel</span>
+                            </span>
+                          </template>
+
+                          <template v-else>
+                            <span
+                              v-html="highlightMatch(record.values.toString())"
+                            ></span>
+                          </template>
+                        </div>
                       </td>
                     </tr>
                   </tbody>
@@ -151,6 +275,7 @@ const error = ref(null);
 const records = ref([]);
 const expanded = ref([]);
 const selectedDomain = ref("hackclub.com.yaml");
+const searchQuery = ref("");
 
 // List of domain files
 const domainFiles = [
@@ -207,7 +332,8 @@ const fetchRecords = async (domain) => {
       records: (Array.isArray(recordList) ? recordList : [recordList]).map(
         (record) => ({
           type: record.type,
-          values: record.values || record.value,
+          ttl: record.ttl,
+          values: record.values || record.value || [],
         })
       ),
     }));
@@ -220,10 +346,68 @@ const fetchRecords = async (domain) => {
   }
 };
 
+const highlightMatch = (text) => {
+  if (!searchQuery.value) return text;
+  const regex = new RegExp(`(${searchQuery.value})`, "gi");
+  return text.replace(
+    regex,
+    '<span class="bg-primary/20 text-primary">$1</span>'
+  );
+};
+
+// Filter and search records
+const filteredRecords = computed(() => {
+  if (!searchQuery.value) return records.value;
+
+  const query = searchQuery.value.toLowerCase();
+
+  return records.value.filter((group) => {
+    // Search in subdomain
+    if (group.subdomain.toLowerCase().includes(query)) return true;
+
+    // Search in records
+    return group.records.some((record) => {
+      // Search in type
+      if (record.type.toLowerCase().includes(query)) return true;
+
+      // Search in values
+      if (Array.isArray(record.values)) {
+        return record.values.some((value) => {
+          if (typeof value === "object") {
+            return Object.values(value).some((v) =>
+              v.toString().toLowerCase().includes(query)
+            );
+          }
+          return value.toString().toLowerCase().includes(query);
+        });
+      }
+
+      return record.values.toString().toLowerCase().includes(query);
+    });
+  });
+});
+
 // Handle domain selection
 const selectDomain = (domain) => {
   selectedDomain.value = domain;
   fetchRecords(domain);
+};
+
+const isVercelDomain = (value) => {
+  return (
+    value &&
+    typeof value === "string" &&
+    value.includes("cname.vercel-dns.com.")
+  );
+};
+
+const isMXRecord = (value) => {
+  return (
+    value &&
+    typeof value === "object" &&
+    ("preference" in value || "priority" in value) &&
+    "exchange" in value
+  );
 };
 
 // Fetch records for the default domain on mount
